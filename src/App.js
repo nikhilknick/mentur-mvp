9b672ab1f40f1b740f898be29d1f74c843fa8870
nikhilknick <nikhilknick@gmail.com>
2019-08-29 14:08:48 +0530

    first com

--- src/App.js
+++ src/App.js
@@ -35,13 +35,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <LambdaDemo />
-        </header>
+       <h1>Hello</h1>
       </div>
     )
   }
